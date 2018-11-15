d1a1f3be5d399025ced4b4aa11c6931e6e0cf579
Gordan Kucinar <gordan.kucinar@avl.com>
2018-11-15 01:13:36 +0100

    Kucinar Gordan

--- exercise/01/animal.h
+++ exercise/01/animal.h
@@ -11,20 +11,6 @@ struct animal
   virtual ~animal() = default;
 };
 
-using animal_id = std::size_t;
-template<animal_id ANIMAL_ID>
-struct animal_name
-{
-  static const std::wstring name;
-};
-
-template<animal_id ANIMAL_ID>
-static std::wstring fail_name() 
-{
-  static_assert(false, "specialize name member");
-}
-template<animal_id ANIMAL_ID> const std::wstring animal_name<ANIMAL_ID>::name = fail_name<ANIMAL_ID>();
-
 template<std::size_t LEGS>
 struct legged_animal : animal
 {
@@ -34,12 +20,26 @@ struct legged_animal : animal
   }
 };
 
+using animal_id = std::size_t;
+template<animal_id ANIMAL_ID>
+struct animal_name
+{
+  static const std::wstring value;
+};
+
+template<animal_id ANIMAL_ID>
+static std::wstring fail_name() 
+{
+  static_assert(false, "specialize name member");
+}
+template<animal_id ANIMAL_ID> const std::wstring animal_name<ANIMAL_ID>::value = fail_name<ANIMAL_ID>();
+
 template<std::size_t ANIMAL_ID, typename ANIMAL_TYPE>
 struct named_animal final : ANIMAL_TYPE
 {
   std::wstring const & species() const noexcept override
   {
-    return animal_name<ANIMAL_ID>::name;
+    return animal_name<ANIMAL_ID>::value;
   }
 };
 
@@ -53,9 +53,9 @@ const animal_id cockroach_id = 1;
 const animal_id sparrow_id = 2;
 const animal_id tarantula_id = 3;
 
-template<> const std::wstring animal_name<sparrow_id>::name = L"sparrow";
-template<> const std::wstring animal_name<cockroach_id>::name = L"cockroach";
-template<> const std::wstring animal_name<tarantula_id>::name = L"tarantula";
+template<> const std::wstring animal_name<sparrow_id>::value = L"sparrow";
+template<> const std::wstring animal_name<cockroach_id>::value = L"cockroach";
+template<> const std::wstring animal_name<tarantula_id>::value = L"tarantula";
 
 using sparrow = named_animal<sparrow_id, bird>;
 using tarantula = named_animal<tarantula_id, spider>;
