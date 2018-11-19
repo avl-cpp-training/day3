32e5d7909a591710d53e21fc8d178d3231a15651
jadranko lucic <jadranko.lucic@avl.com>
2018-11-19 10:46:29 +0100

    some fixes

--- exercise/01/animal.cpp
+++ exercise/01/animal.cpp
@@ -12,32 +12,33 @@ std::wstring cockroach::species() { return  L"cockroach"; }
 unsigned int spider::legs() { return 8; }
 std::wstring tarantula::species() { return L"tarantula"; }
 
-std::wstring leg_counter::add_animal(animal* a)
+std::wstring leg_counter::add_animal(std::unique_ptr<animal> &a)
 {
-  animals.push_back(a);
-  return a->species();
+  std::wstring s = a->species();
+  animals.push_back(std::move(a));
+  return s;
 }
 
 unsigned int leg_counter::legs() const
 {
   unsigned int sum_of_legs = 0;
-  for each (animal* a in animals)
-  {
+  for each (std::unique_ptr<animal> const& a in animals)
+    {
     sum_of_legs += a->legs();
   }
   return sum_of_legs;
 }
 
-animal* animal_factory(int animal_type)
+std::unique_ptr<animal> animal_factory(int animal_type)
 {
   switch (animal_type) {
   case 1:
-    return new cockroach;
+    return std::make_unique<cockroach>();
   case 2:
-    return new sparrow;
+    return std::make_unique<sparrow>();
   case 3:
-    return new tarantula;
-  default: 
+    return std::make_unique<tarantula>();
+  default:
     return nullptr;
   }
 }
--- exercise/01/animal.h
+++ exercise/01/animal.h
@@ -5,7 +5,7 @@
 class animal
 {
 public:
-  animal();//int legs);
+  animal();
   virtual ~animal() = 0;
   virtual std::wstring species() = 0;
   virtual unsigned int legs() = 0;
@@ -50,10 +50,10 @@ public:
 class leg_counter
 {
 private:
-  std::vector<animal*> animals;
+  std::vector<std::unique_ptr<animal>> animals;
 public:
-  std::wstring add_animal(animal* a);
+  std::wstring add_animal(std::unique_ptr<animal> &a);
   unsigned int legs() const;
 };
 
-animal* animal_factory(int animal_type);
+std::unique_ptr<animal> animal_factory(int animal_type);
